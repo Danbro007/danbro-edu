b78f39729859bdc6660c5857e3ddc332bbd429d7
danbro <710170342@qq.com>
2020-12-15 16:17:38 +0800

    1、 添加全局日志处理 Logback

--- commons/common-utils/src/main/java/com/danbro/handler/MyExceptionHandler.java
+++ commons/common-utils/src/main/java/com/danbro/handler/MyExceptionHandler.java
@@ -2,6 +2,7 @@ package com.danbro.handler;
 
 import com.danbro.enums.Result;
 import com.danbro.exception.MyCustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Date 2020/12/15 15:17
  * @Author Danrbo
  */
+@Slf4j
 @ControllerAdvice
 public class MyExceptionHandler {
     @ResponseBody
     @ExceptionHandler(MyCustomException.class)
     public Result error(MyCustomException e) {
+        log.error(e.getMessage());
         return Result.failureOf(e);
     }
 }
